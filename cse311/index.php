2f9c72652ff62834cb79fbab258fb5aa243066ff
TahfeemSiam <tahfeem.siam@gmail.com>
2024-05-31 20:22:12 +0600

    Updated index.php with few sql query  and updated the overall page with more data

--- cse311/index.php
+++ cse311/index.php
@@ -9,7 +9,8 @@
             <?php
 
                 $connect = mysqli_connect("localhost", "root", "", "cse311");
-                $query = "SELECT * FROM products";
+                $query = "SELECT * FROM products WHERE product_category";
+                $query .= " IN('CPU', 'CPU Cooler', 'MoBo', 'GPU', 'RAM', 'PSU', 'HDD', 'SSD', 'Case', 'Casing Cooler') LIMIT 10";
                 $result = mysqli_query($connect, $query);
 
                 while ($product = mysqli_fetch_array($result)) {
@@ -26,7 +27,7 @@
                         <p><?php echo $productPrice ?> BDT</p>
                         <div class="overlay">
                             <h1><?php echo $productName ?></h1>
-                            <h2><?php echo  $productDescription?></h2>
+                            <h2><?php echo  substr($productDescription, 0, 200)?></h2>
                             <a href="details.php?id=<?php echo $productId ?>" class="btn btn-primary">View</a>
                         </div>
                     </div>
@@ -49,6 +50,113 @@
 
         </div>
     </section>
+
+
+
+    <section class="feat">
+        <h2>CPU</h2>
+        <div class="prod-row">
+
+            <?php
+            $query = "SELECT * FROM products WHERE product_category = 'CPU' LIMIT 3";
+            $result = mysqli_query($connect, $query);
+
+            while ($product = mysqli_fetch_array($result)) {
+                $productId = $product["product_id"];
+                $productName = $product["product_name"];
+                $productPrice = $product["product_price"];
+                $productDescription = $product["product_description"];
+                $productImage_1 = $product["image_1"];
+                ?>
+
+                <div class="prod-col">
+                    <img src="./images/<?php echo $productImage_1 ?>">
+                    <h3><?php echo $productName ?></h3>
+                    <p><?php echo $productPrice ?> BDT</p>
+                    <div class="overlay">
+                        <h1><?php echo $productName ?></h1>
+                        <h2><?php echo  substr($productDescription, 0, 200)?></h2>
+                        <a href="details.php?id=<?php echo $productId ?>" class="btn btn-primary">View</a>
+                    </div>
+                </div>
+
+            <?php } ?>
+
+        </div>
+        <a class="btn btn-success" href="category.php?category=CPU">View All</a>
+    </section>
+
+
+
+    <section class="feat">
+        <h2>CPU Cooler</h2>
+        <div class="prod-row">
+
+            <?php
+                $query = "SELECT * FROM products WHERE product_category = 'CPU Cooler' LIMIT 3";
+                $result = mysqli_query($connect, $query);
+
+                while ($product = mysqli_fetch_array($result)) {
+                    $productId = $product["product_id"];
+                    $productName = $product["product_name"];
+                    $productPrice = $product["product_price"];
+                    $productDescription = $product["product_description"];
+                    $productImage_1 = $product["image_1"];
+                    ?>
+
+                    <div class="prod-col">
+                        <img src="./images/<?php echo $productImage_1 ?>">
+                        <h3><?php echo $productName ?></h3>
+                        <p><?php echo $productPrice ?> BDT</p>
+                        <div class="overlay">
+                            <h1><?php echo $productName ?></h1>
+                            <h2><?php echo  substr($productDescription, 0, 200)?></h2>
+                            <a href="details.php?id=<?php echo $productId ?>" class="btn btn-primary">View</a>
+                        </div>
+                    </div>
+
+                <?php } ?>
+
+        </div>
+        <a class="btn btn-success" href="category.php?category=CPU Cooler">View All</a>
+    </section>
+
+
+    <section class="feat">
+        <h2>Motherboard</h2>
+        <div class="prod-row">
+
+            <?php
+            $query = "SELECT * FROM products WHERE product_category = 'MoBo' LIMIT 3";
+            $result = mysqli_query($connect, $query);
+
+            while ($product = mysqli_fetch_array($result)) {
+                $productId = $product["product_id"];
+                $productName = $product["product_name"];
+                $productPrice = $product["product_price"];
+                $productDescription = $product["product_description"];
+                $productImage_1 = $product["image_1"];
+                ?>
+
+                <div class="prod-col">
+                    <img src="./images/<?php echo $productImage_1 ?>">
+                    <h3><?php echo $productName ?></h3>
+                    <p><?php echo $productPrice ?> BDT</p>
+                    <div class="overlay">
+                        <h1><?php echo $productName ?></h1>
+                        <h2><?php echo  substr($productDescription, 0, 200)?></h2>
+                        <a href="details.php?id=<?php echo $productId ?>" class="btn btn-primary">View</a>
+                    </div>
+                </div>
+
+            <?php } ?>
+
+        </div>
+        <a class="btn btn-success" href="category.php?category=MoBo">View All</a>
+    </section>
+
+
+
 
     <?php
         if(isset($_GET['logout'])) {
